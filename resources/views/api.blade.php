3712fe45b4741deb3f152f16e72704b9722b3309
Ryan W. O'Hara <ryan@ryanwohara.com>
2015-04-08 20:41:52 -0400

    Styling & timestamp

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -1,8 +1,15 @@
 @extends('welcome')
 
 @section('listitems')
+	<ol id="workoutlog">
 	@foreach ($items as $item)
-		<li>{{ $item->amount }}x {{ $item->weight }} lbs {{ $item->description }}</li>
+		<li>
+			<span class="translate_up">
+				<span class="medtext">{{ $item->amount }}x {{ $item->weight }} lbs {{ $item->description }}</span>
+				<span class="gray smalltext translate_up">{{ $item->created_at }}</span>
+			</span>
+		</li>
+	</ol>
 	@endforeach
 
 @stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,9 +33,7 @@
 			</div>
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3">
-					<ul id="workoutlog">
-						@yield('listitems')
-					</ul>
+					@yield('listitems')
 				</div>
 			</div>
 		</div>
